b1453c21abdf8f76c0345a07f5642b1762213f84
Travis CI <no-reply@squareup.com>
2017-06-19 21:51:07 +0000

    From connect-api-specification: Update packageVersion to 2.2.0

--- lib/square_connect/api_client.rb
+++ lib/square_connect/api_client.rb
@@ -30,7 +30,7 @@ module SquareConnect
       @config = config
       
       # Construct user agent string. Returns slightly different string for JRuby
-      @user_agent = "Square-Connect-Ruby/2.1.0"
+      @user_agent = "Square-Connect-Ruby/2.2.0"
       
       @default_headers = {
         'Content-Type' => "application/json",
